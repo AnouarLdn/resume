1cdc27d96d7a983536beb470617c143f497b3bc2
Anouar <anouar.haidary@gmail.com>
2020-08-13 11:10:59 +0200

    Back to the top button (done)

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,9 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     const cardimg = document.getElementsByClassName("cardimg");
-//     cardimg.addEventListener("mouseover"), (e) => {
-//         e.preventDefault();
-//         console.log("it works!!!");
-//     }
-// })
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset > 100) {
+        toTop.classList.add('active');
+    } else {
+        toTop.classList.remove("active");
+    }
+    })
